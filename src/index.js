362a1e3daa23411ae7e3d98dec804b36cac9eed5
devildummy <cyrok.13@gmail.com>
2018-09-24 23:41:51 +0300

    Ok, now its works with O(n)

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 module.exports = function longestConsecutiveLength(array) {
-  let max=1;current=1;
+  let max=1,current=1,must={},last;
   if(array.length==0){
     return 0;
   }
-  array.sort(function(a,b){return a-b;});
-  for(let i=1;i<array.length;i++){
-    if(array[i]-array[i-1]==1){
+  array.forEach(function(item,i){must[item]=i;});
+  for(let i=0;i<array.length;i++){
+    if(must[array[i]+1]>=0){
+        if(current<=1){
+          last=i;
+        }
       current++;
-    }else if(array[i]==array[i-1]){
-      current=current;
+      i=must[array[i]+1]-1;
     }else{
+      if(current==1 && !array[i+1]){
+        break;
+      }else if(current!=1){
+        i=last;
+      }
       current=1;
     }
 
