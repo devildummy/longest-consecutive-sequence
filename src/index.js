92412b0fe80d84e544895fb47fcd1b5533e28b2f
devildummy <cyrok.13@gmail.com>
2018-09-24 20:08:29 +0300

    this solution not O(n) complexity, but now i dont understand how to do it, must do it later

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
-}
+  let max=1;current=1;
+  if(array.length==0){
+    return 0;
+  }
+  array.sort(function(a,b){return a-b;});
+  for(let i=1;i<array.length;i++){
+    if(array[i]-array[i-1]==1){
+      current++;
+    }else if(array[i]==array[i-1]){
+      current=current;
+    }else{
+      current=1;
+    }
+
+    if(current>max){
+      max=current;
+    }
+  }
+  return max;
+};
